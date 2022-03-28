904a1a0bd47388083bf44775f60f49978060056f
cicu90 <carlacardozo990@gmail.com>
2022-03-28 17:49:56 +0200

    init backend

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { json } = require("body-parser");
+const helmet = require("helmet");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +14,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(helmet());
+app.use(json());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,12 +34,23 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // console.log("Error Handler Middleware: ");
+  // console.error(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  if (req.headersSent) {
+    // When you add a custom error handler,
+    // you must delegate to the default Express error handler,
+    // when the headers have already been sent to the client:
+    return next(err);
+  }
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500).send({
+    data: null,
+    error: "Something went wrong",
+  });
   res.render('error');
 });
 
